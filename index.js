3fa590acb71c2dc91b71e5b996c223a2a5982279
Hassan Rasheed <hassanrasheed@Hassans-MacBook-Pro.local>
2023-02-07 12:06:32 +0000

    color modes added

--- index.js
+++ index.js
@@ -34,14 +34,33 @@ let movieData = {
     },
   };
   
+let mode = "light"
+const toggleMode = document.querySelector('#toggle-mode');
+toggleMode.addEventListener('click', function(){
+  let content = document.querySelector('.body');
+  if(mode == 'light'){
+    content.classList.remove("light-mode")
+    content.classList.add("dark-mode")
+    mode = "dark";
+  } else{
+    content.classList.remove("dark-mode")
+    content.classList.add("light-mode")
+    mode = "light";
+  }
+})
+
 let movieContainer = document.querySelector(".movie-container");
 for(let key in movieData){
     let movieCard = document.createElement("div")
+    movieCard.classList.add('movie-card')
     movieCard.innerHTML = `
     <h2 class="movie-title">${key}</h2>
     <p class="plot">${movieData[key]['plot']}</p>
+    <div class="extras-container">
     <h3 class="rating">${movieData[key]['rating']}</h3>
     <h3 class="runtime">${movieData[key]['runtime']}</h3>
-    <h3 class="movie-year">${movieData[key]['year']}</h3>`;
+    <h3 class="movie-year">${movieData[key]['year']}</h3>
+    </div>`;
     movieContainer.appendChild(movieCard);
 }
+
